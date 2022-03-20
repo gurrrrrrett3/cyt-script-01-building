9c8eadc7dc337bf94fe69a1c76da78e21d18e7e1
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2022-03-20 13:31:07 -0400

    script complete

---
+++ script.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const yaml = require("js-yaml");
+
+const inDir = "./data-in/";
+const outDir = "./data-out/";
+
+let files = fs.readdirSync(inDir);
+
+files.forEach((file) => {
+  let data = yaml.load(fs.readFileSync(inDir + file, "utf8"));
+
+  if (file == ".lock") return;
+
+  data.options.repeatable = true;
+  data.options.cooldown.enabled = true;
+  data.options.cooldown.time = 10080;
+
+  fs.writeFileSync(outDir + file, yaml.dump(data));
+});
